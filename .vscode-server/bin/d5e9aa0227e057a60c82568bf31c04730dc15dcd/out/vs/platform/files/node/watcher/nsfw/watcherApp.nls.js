541a3b48ba76564d71d144a7eb0f83d98b268d46
fernando henrique <fernandohenrique1206@hotmail.com>
2020-07-13 00:30:41 -0300

    conseguir

---
+++ .vscode-server/bin/d5e9aa0227e057a60c82568bf31c04730dc15dcd/out/vs/platform/files/node/watcher/nsfw/watcherApp.nls.js
@@ -0,0 +1,5 @@
+/*!--------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+define("vs/platform/files/node/watcher/nsfw/watcherApp.nls",{"vs/base/node/processes":["Can't execute a shell command on a UNC drive."],"vs/platform/files/common/files":["Unknown Error","{0}B","{0}KB","{0}MB","{0}GB","{0}TB"]});
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/d5e9aa0227e057a60c82568bf31c04730dc15dcd/core/vs/platform/files/node/watcher/nsfw/watcherApp.nls.js.map
---
+++ .vscode-server/bin/d5e9aa0227e057a60c82568bf31c04730dc15dcd/out/vs/server/uriTransformer.js
@@ -0,0 +1,5 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------------------------------------------*/
+module.exports=function(e){return{transformIncoming:e=>"vscode-remote"===e.scheme?{scheme:"file",path:e.path}:"file"===e.scheme?{scheme:"vscode-local",path:e.path}:e,transformOutgoing:o=>"file"===o.scheme?{scheme:"vscode-remote",authority:e,path:o.path}:"vscode-local"===o.scheme?{scheme:"file",path:o.path}:o,transformOutgoingScheme:e=>"file"===e?"vscode-remote":"vscode-local"===e?"file":e}};
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/d5e9aa0227e057a60c82568bf31c04730dc15dcd/core/vs/server/uriTransformer.js.map
